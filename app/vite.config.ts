4467a74ad4630a809cfcc7a3978bc4f137d7d4b4
winwintoken <71874376+winwintoken@users.noreply.github.com>
2025-08-31 12:17:37 +0800

    vercel fix

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   optimizeDeps: {
     include: ['buffer', 'crypto-browserify', 'stream-browserify', 'path-browserify'],
